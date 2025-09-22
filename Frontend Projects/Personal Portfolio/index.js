a5ad20ee6f9e215e20c7580fe80d1a891f2ca802
Ifram3 <111003934+Ifram3@users.noreply.github.com.>
2025-09-22 06:32:57 -0400

    refactor: make code readable

--- Frontend Projects/Personal Portfolio/index.js
+++ Frontend Projects/Personal Portfolio/index.js
@@ -2,14 +2,13 @@ const toggleSwitch = document.querySelector('.refix input[type="checkbox"]');
 
 function switchTheme(e) {
     if (e.target.checked) {
-        document.documentElement.setAttribute('data-theme', 'dark');
-    }
-    else {
+        document.documentElement.setAttribute("data-theme", "dark");
+    } else {
         document.documentElement.setAttribute('data-theme', 'light')
     }
 }
 
-toggleSwitch.addEventListener('change', switchTheme, false);
+toggleSwitch.addEventListener('change', switchTheme);
 
 
 // function switchTheme(e) {
